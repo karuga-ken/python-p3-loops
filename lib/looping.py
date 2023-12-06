5e5cb475bf49a83966cd170915ccd155c1e79f12
Karuga Mbugua <karugambugua@icloud.com>
2023-12-06 15:52:26 +0300

    completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 11
+    while count > 0:
+        count -= 1
+        print(count)
+        print("Happy New Year!")
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+        return[x*x for x in int_list]
+square_integers([1, 2, 3, 4, 5])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
+fizzbuzz()
